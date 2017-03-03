eb4d7c1a58aac4fa872c844aac0e6deec2fc3d80
wher0001 <richardswheatley@hotmail.com>
2017-03-03 16:32:34 -0500

    fixed maybe?
    maybe fixed it?

--- Image and Video Algorithms/OpenCV and Python/Pitch and Yaw Detection/2_pics_get_diff_opencv_feature_matching_distance.py
+++ Image and Video Algorithms/OpenCV and Python/Pitch and Yaw Detection/2_pics_get_diff_opencv_feature_matching_distance.py
@@ -13,7 +13,7 @@ from matplotlib import pyplot as plt
 
 
 
-def reject_outliers(data, m = 1./2):
+def reject_outliers(data, m=2):
     d = np.abs(data - np.median(data))
     mdev = np.median(d)
     s = d/mdev if mdev else 0.
@@ -21,8 +21,10 @@ def reject_outliers(data, m = 1./2):
 
 def runImageProcessing(img_1, img_2, detector, type, startTime, ref_heading, delta_pixels_h, delta_pixels_w):
     height, width = img_1.shape
-    crop_img1 = img_1[0:height, delta_pixels_w:width]
-    crop_img2 = img_2[0:height, 0:width-delta_pixels_w]
+    print(height, width)
+
+    crop_img1 = img_1[0:height, 0:width]
+    crop_img2 = img_2[0:height, 0:width]
 
     # find the keypoints and descriptors with "detector"
     (kp1, desc1) = detector.detectAndCompute(crop_img1,None)
@@ -43,7 +45,7 @@ def runImageProcessing(img_1, img_2, detector, type, startTime, ref_heading, del
         pts_left_image = []
         pts_right_image = []
         for i, (m,n) in enumerate(matches):
-            if m.distance < 0.4*n.distance:
+            if m.distance < 0.33*n.distance:
                 pts_left_image.append(kp1[m.queryIdx].pt)
                 pts_right_image.append(kp2[m.trainIdx].pt)
 		
@@ -51,39 +53,39 @@ def runImageProcessing(img_1, img_2, detector, type, startTime, ref_heading, del
             pts_left_image = np.float32(pts_left_image)
             pts_right_image = np.float32(pts_right_image)
             
-    mine = pts_left_image - pts_right_image
+        mine = pts_left_image - pts_right_image
 			
-    #mine = reject_outliers(mine)
+        mine = reject_outliers(mine)
 			
-    end = time.time()
-    print(type, " - Number of matches (total):", len(matches), "Number of matches (good):", len(mine), " - Time: ",  end - startTime)
+        end = time.time()
+        print(type, " - Number of matches (total):", len(matches), "Number of matches (good):", len(mine), " - Time: ",  end - startTime)
 
-    if(len(pts_left_image) > 0):
-        pts_left_image = np.float32(pts_left_image)
-        pts_right_image = np.float32(pts_right_image)
-            
-        mine = pts_right_image - pts_left_image
-        my_heading_string  = "delta-heading: " 
-        my_pitch_string    = "delta-pitch:   "
-        my_heading_string2 = "ref'd-heading: "
-        heading = np.average(mine[:, 0])
-        pitch = np.average(mine[:, 1])
-        ref_heading = ref_heading + heading*0.05625
+        if(len(pts_left_image) > 0):
+            pts_left_image = np.float32(pts_left_image)
+            pts_right_image = np.float32(pts_right_image)
+                
+            mine = pts_right_image - pts_left_image
+            my_heading_string  = "delta-heading: " 
+            my_pitch_string    = "delta-pitch:   "
+            my_heading_string2 = "ref'd-heading: "
+            heading = np.average(mine[:, 0])
+            pitch = np.average(mine[:, 1])
+            ref_heading = ref_heading + heading*0.075
 
-    degrees = True
-    if degrees == True:
-        cv2.putText(img_1, my_pitch_string + "{:10.4f}".format(pitch*0.05625), (30,30), cv2.FONT_HERSHEY_COMPLEX_SMALL, 0.8, (0,0,250), 1, cv2.LINE_AA);
-        cv2.putText(img_1, my_heading_string + "{:10.4f}".format(heading*0.05625), (30,60), cv2.FONT_HERSHEY_COMPLEX_SMALL, 0.8, (0,0,250), 1, cv2.LINE_AA);
-        cv2.putText(img_1, my_heading_string2 + "{:10.4f}".format(ref_heading), (30,90), cv2.FONT_HERSHEY_COMPLEX_SMALL, 0.8, (0,0,250), 1, cv2.LINE_AA);
-    else:
-        cv2.putText(img_1, my_pitch_string + "{:10.4f}".format(pitch), (30,30), cv2.FONT_HERSHEY_COMPLEX_SMALL, 0.8, (0,0,250), 1, cv2.LINE_AA);
-        cv2.putText(img_1, my_heading_string + "{:10.4f}".format(heading), (30,60), cv2.FONT_HERSHEY_COMPLEX_SMALL, 0.8, (0,0,250), 1, cv2.LINE_AA);
-        cv2.putText(img_1, my_heading_string2 + "{:10.4f}".format(ref_heading), (30,90), cv2.FONT_HERSHEY_COMPLEX_SMALL, 0.8, (0,0,250), 1, cv2.LINE_AA);
-
-# Draw first 10 matches.
-    img3 = cv2.drawMatchesKnn(crop_img1,kp1,crop_img2,kp2,good[:len(mine)], None, flags=2)
-		
-    return(img3, ref_heading)
+        degrees = True
+        if degrees == True:
+            cv2.putText(img_1, my_pitch_string + "{:10.4f}".format(pitch*0.075), (30,30), cv2.FONT_HERSHEY_COMPLEX_SMALL, 0.8, (0,0,250), 1, cv2.LINE_AA);
+            cv2.putText(img_1, my_heading_string + "{:10.4f}".format(heading*0.075), (30,60), cv2.FONT_HERSHEY_COMPLEX_SMALL, 0.8, (0,0,250), 1, cv2.LINE_AA);
+            cv2.putText(img_1, my_heading_string2 + "{:10.4f}".format(ref_heading), (30,90), cv2.FONT_HERSHEY_COMPLEX_SMALL, 0.8, (0,0,250), 1, cv2.LINE_AA);
+        else:
+            cv2.putText(img_1, my_pitch_string + "{:10.4f}".format(pitch), (30,30), cv2.FONT_HERSHEY_COMPLEX_SMALL, 0.8, (0,0,250), 1, cv2.LINE_AA);
+            cv2.putText(img_1, my_heading_string + "{:10.4f}".format(heading), (30,60), cv2.FONT_HERSHEY_COMPLEX_SMALL, 0.8, (0,0,250), 1, cv2.LINE_AA);
+            cv2.putText(img_1, my_heading_string2 + "{:10.4f}".format(ref_heading), (30,90), cv2.FONT_HERSHEY_COMPLEX_SMALL, 0.8, (0,0,250), 1, cv2.LINE_AA);
+    
+    # Draw first 10 matches.
+        img3 = cv2.drawMatchesKnn(crop_img1,kp1,crop_img2,kp2,good[:len(mine)], None, flags=2)
+		    
+        return(img3, ref_heading)
 
 cv2.ocl.setUseOpenCL(False)
 # Initiate SIFT detector
