43f7a61db48154505a417cc95f3c4e3de80db5a1
wher0001 <richardswheatley@hotmail.com>
2017-03-08 10:20:41 -0500

    fixed python

--- Image and Video Algorithms/OpenCV and Python/Pitch and Yaw Detection/2_pics_get_diff_opencv_feature_matching_distance.py
+++ Image and Video Algorithms/OpenCV and Python/Pitch and Yaw Detection/2_pics_get_diff_opencv_feature_matching_distance.py
@@ -11,13 +11,25 @@ import time
 import easygui
 from matplotlib import pyplot as plt
 
-
-
 def reject_outliers(data, m=2):
     d = np.abs(data - np.median(data))
     mdev = np.median(d)
     s = d/mdev if mdev else 0.
     return data[s<m]
+
+def find_good_matches(matches_regular):
+    good = []
+    for m,n in matches_regular:
+        if m.distance < 0.8*n.distance:
+            good.append([m])
+    return good
+
+def find_better_matches(data):
+    for i, (m,n) in enumerate(matches):
+        if m.distance < 0.33*n.distance:
+            pts_left_image.append(kp1[m.queryIdx].pt)
+            pts_right_image.append(kp2[m.trainIdx].pt)
+    return(pts_left_image, pts_right_image)
 
 def runImageProcessing(img_1, img_2, detector, type, startTime, ref_heading, delta_pixels_h, delta_pixels_w):
     height, width = img_1.shape
@@ -36,35 +48,25 @@ def runImageProcessing(img_1, img_2, detector, type, startTime, ref_heading, del
     # Match descriptors.
     matches = bf.knnMatch(desc1,desc2, k=2);
 
-    good = []
-    for m,n in matches:
-        if m.distance < 0.8*n.distance:
-            good.append([m])
-                
+    good = find_good_matches(matches)
+
     if(len(good) > 0):
         pts_left_image = []
         pts_right_image = []
-        for i, (m,n) in enumerate(matches):
-            if m.distance < 0.33*n.distance:
-                pts_left_image.append(kp1[m.queryIdx].pt)
-                pts_right_image.append(kp2[m.trainIdx].pt)
-		
-        if(len(pts_left_image) > 0):
-            pts_left_image = np.float32(pts_left_image)
-            pts_right_image = np.float32(pts_right_image)
-            
-        mine = pts_left_image - pts_right_image
-			
-        mine = reject_outliers(mine)
-			
-        end = time.time()
-        print(type, " - Number of matches (total):", len(matches), "Number of matches (good):", len(mine), " - Time: ",  end - startTime)
+
+        pts_left_image, pts_right_image = find_better_matches(good)
 
         if(len(pts_left_image) > 0):
             pts_left_image = np.float32(pts_left_image)
             pts_right_image = np.float32(pts_right_image)
-                
-            mine = pts_right_image - pts_left_image
+            
+            mine = pts_left_image - pts_right_image
+
+            mine = reject_outliers(mine)
+
+            end = time.time()
+            print(type, " - Number of matches (total):", len(matches), "Number of matches (good):", len(mine), " - Time: ",  end - startTime)
+
             my_heading_string  = "delta-heading: " 
             my_pitch_string    = "delta-pitch:   "
             my_heading_string2 = "ref'd-heading: "
@@ -72,46 +74,50 @@ def runImageProcessing(img_1, img_2, detector, type, startTime, ref_heading, del
             pitch = np.average(mine[:, 1])
             ref_heading = ref_heading + heading*0.075
 
-        degrees = True
-        if degrees == True:
-            cv2.putText(img_1, my_pitch_string + "{:10.4f}".format(pitch*0.075), (30,30), cv2.FONT_HERSHEY_COMPLEX_SMALL, 0.8, (0,0,250), 1, cv2.LINE_AA);
-            cv2.putText(img_1, my_heading_string + "{:10.4f}".format(heading*0.075), (30,60), cv2.FONT_HERSHEY_COMPLEX_SMALL, 0.8, (0,0,250), 1, cv2.LINE_AA);
-            cv2.putText(img_1, my_heading_string2 + "{:10.4f}".format(ref_heading), (30,90), cv2.FONT_HERSHEY_COMPLEX_SMALL, 0.8, (0,0,250), 1, cv2.LINE_AA);
-        else:
-            cv2.putText(img_1, my_pitch_string + "{:10.4f}".format(pitch), (30,30), cv2.FONT_HERSHEY_COMPLEX_SMALL, 0.8, (0,0,250), 1, cv2.LINE_AA);
-            cv2.putText(img_1, my_heading_string + "{:10.4f}".format(heading), (30,60), cv2.FONT_HERSHEY_COMPLEX_SMALL, 0.8, (0,0,250), 1, cv2.LINE_AA);
-            cv2.putText(img_1, my_heading_string2 + "{:10.4f}".format(ref_heading), (30,90), cv2.FONT_HERSHEY_COMPLEX_SMALL, 0.8, (0,0,250), 1, cv2.LINE_AA);
-    
-    # Draw first 10 matches.
-        img3 = cv2.drawMatchesKnn(crop_img1,kp1,crop_img2,kp2,good[:len(mine)], None, flags=2)
-		    
-        return(img3, ref_heading)
+            # If we want different units
+            units = "pixels"
 
-cv2.ocl.setUseOpenCL(False)
-# Initiate SIFT detector
-# Initiate SIFT detector
-orb = cv2.ORB_create()
+            # Draw matches that we accept
+            img3 = cv2.drawMatchesKnn(crop_img1,kp1,crop_img2,kp2,good[:len(mine)], None, flags=2)
 
-reference_heading = 180.0
+            if units == "degrees":
+                cv2.putText(img3, my_pitch_string + "{:10.4f}".format(pitch*0.075), (30,30), cv2.FONT_HERSHEY_COMPLEX_SMALL, 0.8, (0,0,250), 1, cv2.LINE_AA);
+                cv2.putText(img3, my_heading_string + "{:10.4f}".format(heading*0.075), (30,60), cv2.FONT_HERSHEY_COMPLEX_SMALL, 0.8, (0,0,250), 1, cv2.LINE_AA);
+                cv2.putText(img3, my_heading_string2 + "{:10.4f}".format(ref_heading), (30,90), cv2.FONT_HERSHEY_COMPLEX_SMALL, 0.8, (0,0,250), 1, cv2.LINE_AA);
+            elif units == "mils":
+                cv2.putText(img3, my_pitch_string + "{:10.4f}".format(pitch), (30,30), cv2.FONT_HERSHEY_COMPLEX_SMALL, 0.8, (0,0,250), 1, cv2.LINE_AA);
+                cv2.putText(img3, my_heading_string + "{:10.4f}".format(heading), (30,60), cv2.FONT_HERSHEY_COMPLEX_SMALL, 0.8, (0,0,250), 1, cv2.LINE_AA);
+                cv2.putText(img3, my_heading_string2 + "{:10.4f}".format(ref_heading), (30,90), cv2.FONT_HERSHEY_COMPLEX_SMALL, 0.8, (0,0,250), 1, cv2.LINE_AA);
+            else # This is for Pixel Measurements
+                cv2.putText(img3, "delta-pixels-pitch: " + "{:10.4f}".format(pitch), (30,30), cv2.FONT_HERSHEY_COMPLEX_SMALL, 0.8, (0,0,250), 1, cv2.LINE_AA);
+                cv2.putText(img3, "delta-pixels-yaw: " + "{:10.4f}".format(heading), (30,60), cv2.FONT_HERSHEY_COMPLEX_SMALL, 0.8, (0,0,250), 1, cv2.LINE_AA);
 
-img1 = cv2.imread(easygui.fileopenbox(), 0)
-img2 = cv2.imread(easygui.fileopenbox(), 0)
+            return(img3, ref_heading)
+    return(img_2, 999.99)
 
-start = time.time()
-done_frame, reference_heading = runImageProcessing(img1.copy(), img2.copy(), cv2.ORB_create(nfeatures=2000), "ORB", start, reference_heading, 0, 267)
+def main():
+    cv2.ocl.setUseOpenCL(False)
 
+    # Initiate ORB detector
+    orb = cv2.ORB_create()
 
-cv2.imshow("finished", done_frame)
-print(reference_heading)
-cv2.imwrite("done_frame_matches.jpg", done_frame)
+    reference_heading = 180.0
 
-while(1):
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+    img1 = cv2.imread(easygui.fileopenbox(), 0)
+    img2 = cv2.imread(easygui.fileopenbox(), 0)
 
+    start = time.time()
+    done_frame, reference_heading = runImageProcessing(img1.copy(), img2.copy(), cv2.ORB_create(nfeatures=2000), "ORB", start, reference_heading, 0, 267)
 
-cv2.destroyAllWindows()
+    cv2.imshow("finished", done_frame)
+    print(reference_heading)
+    cv2.imwrite("done_frame_matches.jpg", done_frame)
 
+    while(1):
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 
+    cv2.destroyAllWindows()
 
-
+if __name__ == '__main__':
+    main()
